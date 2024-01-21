324f8d0fa63b37c0a0676d8a356c760df774cb6a
damocles <lunahackley@gmail.com>
2024-01-20 16:22:45 -0800

    Create framework.js

---
+++ ross/framework.js
@@ -0,0 +1,27 @@
+import Document from './document.js'
+import {
+  Element,
+  Rect,
+  RoundRect,
+  Circle,
+  Arc,
+  Line,
+  Bar,
+  Text,
+  Poly,
+  Clip,
+} from './elements.js'
+
+export {
+  Document,
+  Element,
+  Rect,
+  RoundRect,
+  Circle,
+  Arc,
+  Line,
+  Bar,
+  Text,
+  Poly,
+  Clip,
+}
