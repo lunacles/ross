c9e8d8385487338acfc1890192572ae2c73e199c
damocles <lunahackley@gmail.com>
2024-01-24 15:31:04 -0800

    Add clickregion

---
+++ rossjs/components/clickregion.js
@@ -0,0 +1,41 @@
+import { mouse } from '../event.js'
+
+import {
+  Rect
+} from '../elements.js'
+
+const ClickRegion = class {
+  static create({ x = 0, y = 0, width = 0, height = 0 } = {}) {
+    return new ClickRegion(x, y, width, height)
+  }
+  constructor(x, y, width, height) {
+    this.x = x
+    this.y = y
+    this.width = width
+    this.height = height
+
+    this.active = true
+  }
+  update({ x = 0, y = 0, width = 0, height = 0, debug = false } = {}) {
+    this.x = x
+    this.y = y
+    this.width = width
+    this.height = height
+
+    if (debug) {
+      Rect.draw({
+        x: x, y,
+        width, height
+      }).stroke('#ff0000', 1)
+    }
+  }
+  check() {
+    if (!this.active) return false
+    return mouse.x >= this.x && mouse.x < this.x + this.width && mouse.y >= this.y && mouse.y < this.y + this.height
+  }
+  toggle(state) {
+    this.active = state
+  }
+}
+
+export default ClickRegion
