47b185deeae44d3d654641148f2f847a2f6cbaf3
damocles <lunahackley@gmail.com>
2024-01-30 17:23:52 -0800

    Add onUpdate function

--- rossjs/components/button.js
+++ rossjs/components/button.js
@@ -7,13 +7,14 @@ import ClickRegion from './clickregion.js'
 import { Interaction } from '../event.js'
 
 const Button = class extends Element {
-  static create() {
-    return new Button()
+  static create({ defaultState = false, onUpdate = () => {} } = {}) {
+    return new Button(defaultState, onUpdate)
   }
-  constructor() {
+  constructor(defaultState, onUpdate) {
     super()
 
-    this.state = false
+    this.state = defaultState
+    this.onUpdate = onUpdate
 
     this.x = 0
     this.y = 0
@@ -22,20 +23,23 @@ const Button = class extends Element {
 
     this.clickRegion = ClickRegion.create()
   }
-  draw({ x = 0, y = 0, width = 0, height = 0 }) {
+  update({ x = 0, y = 0, width = 0, height = 0 }) {
     this.x = x
     this.y = y
     this.width = width
     this.height = height
 
     this.clickRegion.update({
-      x: this.x - this.width * 0.5, y: this.y - this.height * 0.5,
-      width: this.width * 2, height: this.height * 2,
+      x: this.x, y: this.y,
+      width: this.width, height: this.height,
     })
 
     if (this.clickRegion.check() && Interaction.left) {
       this.state = !this.state
+      this.onUpdate(this.state)
     }
+
+    return this
   }
 }
 
