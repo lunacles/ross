6baedd2a6d2f736bbf5f7bd7416b772f572cae52
damocles <lunahackley@gmail.com>
2024-03-07 18:08:50 -0500

    Remove useless utility

--- rossjs/rossutils.js
+++ rossjs/rossutils.js
@@ -1,16 +1,3 @@
-export const mixColors = (hex1, hex2, weight2 = 0.5) => {
-  if (weight2 <= 0) return hex1
-  if (weight2 >= 1) return hex2
-  let weight1 = 1 - weight2
-  let int1 = parseInt(hex1.slice(1, 7), 16)
-  let int2 = parseInt(hex2.slice(1, 7), 16)
-  let int =
-    (((int1 & 0xff0000) * weight1 + (int2 & 0xff0000) * weight2) & 0xff0000) |
-    (((int1 & 0x00ff00) * weight1 + (int2 & 0x00ff00) * weight2) & 0x00ff00) |
-    (((int1 & 0x0000ff) * weight1 + (int2 & 0x0000ff) * weight2) & 0x0000ff)
-  return '#' + int.toString(16).padStart(6, '0')
-}
-
 export const clamp = (value, min, max) => Math.min(Math.max(value, min), max)
 
 export const sleep = ms => new Promise(r => setTimeout(r, ms))
